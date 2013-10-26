5c3383d662ead465f47436d6186d4de77e999e9c
mamuya <mamuya325@gmail.com>
2013-10-26 17:30:45 +0700

    update

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '1234';
-$db['default']['database'] = 'posthaidb';
+$db['default']['password'] = '';
+$db['default']['database'] = 'it-techz';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
