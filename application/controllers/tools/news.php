ad6bbcd61aeca95a524b72947d9225d34c5076c0
windstruck <nay_issada@hotmail.com>
2013-10-27 18:36:33 +0700

    update from mamuya

--- application/controllers/tools/news.php
+++ application/controllers/tools/news.php
@@ -10,11 +10,13 @@ class News extends CI_Controller {
        }	
 	   
 	public function index(){	
+		$this->load->view('tools/header_tool');
 		$this->load->view('tools/news/addnews');
+		$this->load->view('tools/footer_tool');
 	}
 	
 	function addCat(){
-	$CPOST = $this->input->post();	
+		$CPOST = $this->input->post();	
     
 		$dataMC['name'] = $CPOST['name'];		
 		$dataMC['description'] = $CPOST['desc'];		
@@ -28,8 +30,16 @@ class News extends CI_Controller {
 		echo $this->newsModel->add_category($dataAll);
 	}
 	
+
 	public function addImages(){
 		$this->load->view('tools/news/addImages');
 	}
+	function addArticle(){
+		$this->load->view('tools/header_tool');
+		$this->load->view('tools/news/addarticle');	
+		$this->load->view('tools/footer_tool');
+		
+	}
 }
+
 ?>
